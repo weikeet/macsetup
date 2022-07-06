1690c4613230eb8a6637b33c7c525442cb022cc5
zhengwei <zhengwei.zhang@enerjoy.life>
2022-07-06 18:11:00 +0800

    fixed open jdk install

--- env/open_jdk_install.sh
+++ env/open_jdk_install.sh
@@ -11,7 +11,10 @@ if [[ ! -e ~/Development ]]; then
     mkdir ~/Development
 fi
 
+cd ~/Development
+
 microsoft_11_jdk=/Library/Java/JavaVirtualMachines/microsoft-11.jdk
+
 if [[ ! -e $microsoft_11_jdk ]]; then
     jdk_download_url=https://aka.ms/download-jdk/
     if is_arm_cpu; then
@@ -21,11 +24,13 @@ if [[ ! -e $microsoft_11_jdk ]]; then
     fi
 
     print_green "download $jdk_download_url$jdk_file_name"
+    wget $jdk_download_url$jdk_file_name
+    tar -zxvf $jdk_file_name
+
     jdk_file_name_unzip=jdk-11.0.15+10
-    wget -P ~/Development $jdk_download_url$jdk_file_name && tar -zxvf $jdk_file_name
 
     print_green "install $microsoft_11_jdk"
-    echo $USER_PWD | sudo -S mv ~/Development/$jdk_file_name_unzip $microsoft_11_jdk
+    echo $USER_PWD | sudo -S mv $jdk_file_name_unzip $microsoft_11_jdk
     rm $jdk_file_name
 else
     print_yellow "You have installed microsoft-11.jdk"
