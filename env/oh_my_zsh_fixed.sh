0f0bf782f67a546cb0f9d08385f5d77712013b16
zhengwei <zhengwei.zhang@enerjoy.life>
2022-07-06 18:11:11 +0800

    add oh my zsh fixed

---
+++ env/oh_my_zsh_fixed.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# issue: Zsh detects insecure completion-dependent directories
+# links: https://stackoverflow.com/questions/61433167/zsh-detects-insecure-completion-dependent-directories
+
+# usage: bash ~/.macsetup/env/oh_my_zsh_fixed.sh
+
+chmod 755 /usr/local/share/zsh
+chmod 755 /usr/local/share/zsh/site-functions
